bf3f270ec4e86e1da5e1fc7fcd6f8e51b31a2b2e
a270639090 <69082802+a270639090@users.noreply.github.com>
2021-08-05 11:04:11 +0800

    husky使用

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     "@vue/typescript/recommended",
     "@vue/prettier",
     "@vue/prettier/@typescript-eslint",
-    "plugin:perttier/recommended"
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
     ecmaVersion: 2020
